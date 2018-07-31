4fa247754fb5f2ce828e16e1a8932aea436d2d7b
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-07-31 11:56:00 +0200

    Add contant for the location of the test train registry

---
+++ library/src/main/kotlin/de/difuture/ekut/pht/test/lib/constants.kt
@@ -0,0 +1,10 @@
+package de.difuture.ekut.pht.test.lib
+
+
+/**
+ * The train registry that is going to be used for all the test that require
+ * a train registry with some trains. This image should be hosted on a publicly
+ * available registry, such as Docker Hub.
+ *
+ */
+const val TEST_TRAIN_REGISTRY_REPOSITORY = "lukaszimmermann/pht-test-train-registry:latest"
