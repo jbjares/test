35df823bc823044883be393f982b0051c5b5b2fa
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-08-07 10:50:32 +0200

    Update with list of all test trains

--- library/src/main/kotlin/de/difuture/ekut/pht/test/lib/Train.kt
+++ library/src/main/kotlin/de/difuture/ekut/pht/test/lib/Train.kt
@@ -8,3 +8,13 @@ const val TRAIN_TEST_CHECK_REQUIREMENTS_STATUS_2 = "train_test_check_requirement
 const val TRAIN_TEST_PRINT_SUMMARY_1 = "train_test_print_summary_1"
 const val TRAIN_TEST_PRINT_SUMMARY_2 = "train_test_print_summary_2"
 const val TRAIN_TEST_COUNT_ROWS = "train_test_count_rows"
+
+
+val TRAINS_TEST_ALL = listOf(
+        TRAIN_TEST_CHECK_REQUIREMENTS_STATUS_0,
+        TRAIN_TEST_CHECK_REQUIREMENTS_STATUS_1,
+        TRAIN_TEST_CHECK_REQUIREMENTS_STATUS_2,
+        TRAIN_TEST_PRINT_SUMMARY_1,
+        TRAIN_TEST_PRINT_SUMMARY_2,
+        TRAIN_TEST_COUNT_ROWS
+)
